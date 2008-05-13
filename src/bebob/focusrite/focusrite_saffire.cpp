ojsl1/libffado

commit 1cc0709559a02e60259b0fdf5b8414f4a37ebfa0

impement level control for saffire LE outputs (cfr ticket #56)
git-svn-id: http://subversion.ffado.org/ffado/trunk/libffado@1167 2be59082-3212-0410-8809-b0798e1608f0

src/bebob/focusrite/focusrite_saffire.cpp
@@ -98,6 +98,49 @@ SaffireDevice::buildMixer()
             new BinaryControl(*this, 
                     FR_SAFFIRELE_CMD_ID_BITFIELD_OUT56, FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_MUTE,
                     "Out56Mute", "Out5/6 Mute", "Output 5/6 Mute"));
+
+        // output mute controls
+        result &= m_MixerContainer->addElement(
+            new BinaryControl(*this, 
+                    FR_SAFFIRELE_CMD_ID_BITFIELD_OUT12, FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_MUTE,
+                    "Out12Mute", "Out1/2 Mute", "Output 1/2 Mute"));
+        result &= m_MixerContainer->addElement(
+            new BinaryControl(*this, 
+                    FR_SAFFIRELE_CMD_ID_BITFIELD_OUT34, FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_MUTE,
+                    "Out34Mute", "Out3/4 Mute", "Output 3/4 Mute"));
+        result &= m_MixerContainer->addElement(
+            new BinaryControl(*this, 
+                    FR_SAFFIRELE_CMD_ID_BITFIELD_OUT56, FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_MUTE,
+                    "Out56Mute", "Out5/6 Mute", "Output 5/6 Mute"));
+
+        // output front panel hw volume control
+        result &= m_MixerContainer->addElement(
+            new BinaryControl(*this, 
+                    FR_SAFFIRELE_CMD_ID_BITFIELD_OUT12, FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_HWCTRL,
+                    "Out12HwCtrl", "Out1/2 HwCtrl", "Output 1/2 Front Panel Hardware volume control"));
+        result &= m_MixerContainer->addElement(
+            new BinaryControl(*this, 
+                    FR_SAFFIRELE_CMD_ID_BITFIELD_OUT34, FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_HWCTRL,
+                    "Out34HwCtrl", "Out3/4 HwCtrl", "Output 3/4 Front Panel Hardware volume control"));
+        result &= m_MixerContainer->addElement(
+            new BinaryControl(*this, 
+                    FR_SAFFIRELE_CMD_ID_BITFIELD_OUT56, FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_HWCTRL,
+                    "Out56HwCtrl", "Out5/6 HwCtrl", "Output 5/6 Front Panel Hardware volume control"));
+
+        // output level controls
+        result &= m_MixerContainer->addElement(
+            new VolumeControlLowRes(*this, 
+                    FR_SAFFIRELE_CMD_ID_BITFIELD_OUT12, FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_DAC,
+                    "Out12Level", "Out1/2 Level", "Output 1/2 Level"));
+        result &= m_MixerContainer->addElement(
+            new VolumeControlLowRes(*this, 
+                    FR_SAFFIRELE_CMD_ID_BITFIELD_OUT34, FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_DAC,
+                    "Out34Level", "Out3/4 Level", "Output 3/4 Level"));
+        result &= m_MixerContainer->addElement(
+            new VolumeControlLowRes(*this, 
+                    FR_SAFFIRELE_CMD_ID_BITFIELD_OUT56, FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_DAC,
+                    "Out56Level", "Out5/6 Level", "Output 5/6 Level"));
+
     } else {
         // create control objects for the saffire
         result &= m_MixerContainer->addElement(
@@ -154,19 +197,19 @@ SaffireDevice::buildMixer()
         // output level controls
         result &= m_MixerContainer->addElement(
             new VolumeControlLowRes(*this, 
-                    FR_SAFFIRE_CMD_ID_BITFIELD_OUT12, 0,
+                    FR_SAFFIRE_CMD_ID_BITFIELD_OUT12, FR_SAFFIRE_CMD_ID_BITFIELD_BIT_DAC,
                     "Out12Level", "Out1/2 Level", "Output 1/2 Level"));
         result &= m_MixerContainer->addElement(
             new VolumeControlLowRes(*this, 
-                    FR_SAFFIRE_CMD_ID_BITFIELD_OUT34, 0,
+                    FR_SAFFIRE_CMD_ID_BITFIELD_OUT34, FR_SAFFIRE_CMD_ID_BITFIELD_BIT_DAC,
                     "Out34Level", "Out3/4 Level", "Output 3/4 Level"));
         result &= m_MixerContainer->addElement(
             new VolumeControlLowRes(*this, 
-                    FR_SAFFIRE_CMD_ID_BITFIELD_OUT56, 0,
+                    FR_SAFFIRE_CMD_ID_BITFIELD_OUT56, FR_SAFFIRE_CMD_ID_BITFIELD_BIT_DAC,
                     "Out56Level", "Out5/6 Level", "Output 5/6 Level"));
         result &= m_MixerContainer->addElement(
             new VolumeControlLowRes(*this, 
-                    FR_SAFFIRE_CMD_ID_BITFIELD_OUT78, 0,
+                    FR_SAFFIRE_CMD_ID_BITFIELD_OUT78, FR_SAFFIRE_CMD_ID_BITFIELD_BIT_DAC,
                     "Out78Level", "Out7/8 Level", "Output 7/8 Level"));
     }
     

src/bebob/focusrite/focusrite_saffire.h
@@ -103,6 +103,7 @@
 #define FR_SAFFIRE_CMD_ID_BITFIELD_BIT_DIM       24
 #define FR_SAFFIRE_CMD_ID_BITFIELD_BIT_MUTE      25
 #define FR_SAFFIRE_CMD_ID_BITFIELD_BIT_HWCTRL    26
+#define FR_SAFFIRE_CMD_ID_BITFIELD_BIT_DAC        0
 
 // other stuff
 #define FR_SAFFIRE_CMD_ID_MONITOR_DIAL    61
@@ -233,6 +234,7 @@
 
 #define FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_MUTE      25
 #define FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_HWCTRL    26
+#define FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_DAC        0
 
 #define FR_SAFFIRELE_CMD_ID_EXT_CLOCK_LOCK       108
 #define FR_SAFFIRELE_CMD_ID_AUDIO_ON             109

support/mixer/mixer_saffirele.py
@@ -92,8 +92,11 @@ class SaffireLEMixer(SaffireLEMixerUI):
 
             self.SelectorControls={
                     self.chkOut12Mute:          ['/Mixer/Out12Mute'],
+                    self.chkOut12HwCtrl:        ['/Mixer/Out12HwCtrl'],
                     self.chkOut34Mute:          ['/Mixer/Out34Mute'],
+                    self.chkOut34HwCtrl:        ['/Mixer/Out34HwCtrl'],
                     self.chkOut56Mute:          ['/Mixer/Out56Mute'],
+                    self.chkOut56HwCtrl:        ['/Mixer/Out56HwCtrl'],
                     self.chkSPDIFTransparent:   ['/Mixer/SpdifTransparent'],
                     self.chkMIDITru:            ['/Mixer/MidiThru'],
                     self.chkHighGain3:          ['/Mixer/HighGainLine3'],
@@ -101,6 +104,9 @@ class SaffireLEMixer(SaffireLEMixerUI):
                     }
 
             self.VolumeControlsLowRes={
+                    self.sldOut12Level:      ['/Mixer/Out12Level'],
+                    self.sldOut34Level:      ['/Mixer/Out34Level'],
+                    self.sldOut56Level:      ['/Mixer/Out56Level'],
                     }
 
     def updateMatrixVolume(self,a0):

commit 0f8a97e94bc89dd4e52daac1488fef80d49cd3b5

fix bug introduced in r1167
git-svn-id: http://subversion.ffado.org/ffado/trunk/libffado@1170 2be59082-3212-0410-8809-b0798e1608f0

src/bebob/focusrite/focusrite_saffire.cpp
@@ -99,20 +99,6 @@ SaffireDevice::buildMixer()
                     FR_SAFFIRELE_CMD_ID_BITFIELD_OUT56, FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_MUTE,
                     "Out56Mute", "Out5/6 Mute", "Output 5/6 Mute"));
 
-        // output mute controls
-        result &= m_MixerContainer->addElement(
-            new BinaryControl(*this, 
-                    FR_SAFFIRELE_CMD_ID_BITFIELD_OUT12, FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_MUTE,
-                    "Out12Mute", "Out1/2 Mute", "Output 1/2 Mute"));
-        result &= m_MixerContainer->addElement(
-            new BinaryControl(*this, 
-                    FR_SAFFIRELE_CMD_ID_BITFIELD_OUT34, FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_MUTE,
-                    "Out34Mute", "Out3/4 Mute", "Output 3/4 Mute"));
-        result &= m_MixerContainer->addElement(
-            new BinaryControl(*this, 
-                    FR_SAFFIRELE_CMD_ID_BITFIELD_OUT56, FR_SAFFIRELE_CMD_ID_BITFIELD_BIT_MUTE,
-                    "Out56Mute", "Out5/6 Mute", "Output 5/6 Mute"));
-
         // output front panel hw volume control
         result &= m_MixerContainer->addElement(
             new BinaryControl(*this, 