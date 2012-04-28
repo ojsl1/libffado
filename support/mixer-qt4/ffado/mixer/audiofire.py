ojsl1/libffado

commit 4961120785d5f542e7e011e7d1b0a1715c3ca09d

audiofire: first attempt at disabling the save-settings button when streaming is active.  Evidence suggests the hardware does not cope well when this is done with streaming active.  This change has not been tested since I don't have an audiofire device; if I've broken the audiofire mixer with this commit please let me know.
git-svn-id: http://subversion.ffado.org/ffado/trunk/libffado@2129 2be59082-3212-0410-8809-b0798e1608f0

support/mixer-qt4/ffado/mixer/audiofire.py
@@ -20,7 +20,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from PyQt4.QtCore import SIGNAL, SLOT, QObject, Qt
+from PyQt4.QtCore import SIGNAL, SLOT, QObject, Qt, QTimer
 from PyQt4.QtGui import QWidget, QHBoxLayout, QVBoxLayout, \
                         QGroupBox, QTabWidget, QLabel, \
                         QPushButton, QToolButton, QSpacerItem, QSizePolicy
@@ -283,6 +283,19 @@ class AudioFire(QWidget):
         self.SPDIFmodeControls[settings.radioConsumer] = ["/SpdifMode", 0]
         self.SPDIFmodeControls[settings.radioProfessional] = ["/SpdifMode", 1]
 
+    def polledUpdate(self):
+        ss = self.streamingstatus.selected()
+
+        # Only alter controls sensitive to the streaming state when the
+        # streaming state has changed.
+        if (ss != self.streaming_state):
+            ss_txt = self.streamingstatus.getEnumLabel(ss)
+            # The device doesn't cope very well if "save settings" is done
+            # while streaming is active
+            settings.btnSaveSettings.setEnabled(ss_txt=='Idle')
+
+        self.streaming_state = ss
+
     def initValues(self):
         log.debug("Init values")
 
@@ -358,5 +371,10 @@ class AudioFire(QWidget):
 
             # connect the UI element
             QObject.connect(ctrl,SIGNAL('toggled(bool)'),self.updateSPDIFmodeControl)
+
+        self.update_timer = QTimer(self)
+        QObject.connect(self.update_timer, SIGNAL('timeout()'), self.polledUpdate)
+        self.update_timer.start(1000)
+        self.streaming_state = -1
 
 # vim: et

commit 54d6154638a9e56020fcbe0023f3939cf715bd97

audiofire: possibly fix a bug introduced in r2129.  This needs testing by someone with an audiofire.
git-svn-id: http://subversion.ffado.org/ffado/trunk/libffado@2130 2be59082-3212-0410-8809-b0798e1608f0

support/mixer-qt4/ffado/mixer/audiofire.py
@@ -283,6 +283,9 @@ class AudioFire(QWidget):
         self.SPDIFmodeControls[settings.radioConsumer] = ["/SpdifMode", 0]
         self.SPDIFmodeControls[settings.radioProfessional] = ["/SpdifMode", 1]
 
+        # Store a reference to the "save" button for later manipulation
+        self.btnSaveSettings = settings.btnSaveSettings
+
     def polledUpdate(self):
         ss = self.streamingstatus.selected()
 
@@ -292,7 +295,7 @@ class AudioFire(QWidget):
             ss_txt = self.streamingstatus.getEnumLabel(ss)
             # The device doesn't cope very well if "save settings" is done
             # while streaming is active
-            settings.btnSaveSettings.setEnabled(ss_txt=='Idle')
+            self.btnSaveSettings.setEnabled(ss_txt=='Idle')
 
         self.streaming_state = ss
 