ojsl1/libffado

commit c6a243941e069285a71c0cf5785d3aafde533e62

This is a first pass at converting the signal handling of ffado-mixer to the new style implemented by pyqtSignal[1].  It is based on patches provided by Takashi Sakamoto in January 2015[2], Xavier Forestier in November 2016[3] and additional minor fixes.
Note that this patchset breaks the matrix mixer due to problems with the
treatment of the valueChanged signal from the MixerNode class.  More work is
needed to determine what the problem is and how best to fix it in the
context of the new signal handling.

[1] http://www.riverbankcomputing.co.uk/news/pyqt-45
[2] https://sourceforge.net/p/ffado/mailman/ffado-devel/thread/1421471004-13162-1-git-send-email-o-takashi%40sakamocchi.jp/#msg33244745
[3] https://sourceforge.net/p/ffado/mailman/message/35457569/



git-svn-id: http://subversion.ffado.org/ffado/trunk/libffado@2653 2be59082-3212-0410-8809-b0798e1608f0

support/mixer-qt4/ffado/mixer/saffire_dice.py
@@ -89,14 +89,14 @@ class Saffire_Dice(Generic_Dice_EAP):
         # Global settings
         self.muteInterface = BooleanControl(self.hw, self.hw.basepath+"/EAP/Monitoring/GlobalMute/State")
         widget.GlobalMute.setChecked(self.muteInterface.selected())
-        self.connect(widget.GlobalMute, QtCore.SIGNAL("toggled(bool)"), self.muteToggle)
+        widget.GlobalMute.toggled.connect(self.muteToggle)
 
         self.dimInterface = BooleanControl(self.hw, self.hw.basepath+"/EAP/Monitoring/GlobalDim/State")
         widget.GlobalDim.setChecked(self.dimInterface.selected())
-        self.connect(widget.GlobalDim, QtCore.SIGNAL("toggled(bool)"), self.dimToggle)
+        widget.GlobalDim.toggled.connect(self.dimToggle)
         self.dimLevelInterface = DiscreteControl(self.hw, self.hw.basepath+"/EAP/Monitoring/GlobalDim/Level")
         widget.DimLevel.setValue(self.dimLevelInterface.getvalue())
-        self.connect(widget.DimLevel, QtCore.SIGNAL("valueChanged(int)"), self.dimLevelChanged)
+        widget.DimLevel.valueChanged.connect(self.dimLevelChanged)
         self.DimLevel = widget.DimLevel
         widget.DimLevel.setEnabled(self.dimInterface.selected())
 
@@ -257,7 +257,7 @@ class Saffire_Dice(Generic_Dice_EAP):
             # Adat/Spdif switch control from interface for Pro40
             self.adatSpdifInterface = BooleanControl(self.hw, self.hw.basepath+"/EAP/Monitoring/AdatSpdif/State")
             widget.AdatSpdif.setChecked(self.adatSpdifInterface.selected())
-            self.connect(widget.AdatSpdif, QtCore.SIGNAL("toggled(bool)"), self.adatSpdifToggle)
+            widget.AdatSpdif.toggled.connect(self.adatSpdifToggle)
 
         if ModelName in ("SAFFIRE_PRO_14", "SAFFIRE_PRO_24"):
             # Line/Inst and Hi/Lo switches for Pro14 and 24
@@ -273,7 +273,7 @@ class Saffire_Dice(Generic_Dice_EAP):
             self.LineInSwitches.append(p)
             for i in range(4):
                 self.LineInSwitches[i].widget.setChecked(self.LineInSwitches[i].Interface.selected())
-                self.connect(self.LineInSwitches[i].widget, QtCore.SIGNAL("toggled(bool)"), self.LineInSwitches[i].Interface.select)
+                self.LineInSwitches[i].widget.toggled.connect(self.LineInSwitches[i].Interface.select)
             widget.LineInSwitchLine_1.setChecked(not self.LineInSwitches[0].Interface.selected())
             widget.LineInSwitchLine_2.setChecked(not self.LineInSwitches[1].Interface.selected())
             widget.LineInSwitchLo_3.setChecked(not self.LineInSwitches[2].Interface.selected())
@@ -283,24 +283,23 @@ class Saffire_Dice(Generic_Dice_EAP):
         if ModelName != "SAFFIRE_PRO26":
             for i in range(self.nbLines/2):
                 self.LineMonos[i].widget.setChecked(self.LineMonos[i].Interface.selected())
-                self.connect(self.LineMonos[i].widget, QtCore.SIGNAL("toggled(bool)"), self.LineMonos[i].Interface.select)
-
+                self.LineMonos[i].toggled.connect(self.LineMonos[i].Interface.select)
 
         for i in range(self.nbLines):
             self.LineUnActivates[i].widget.setChecked(self.LineUnActivates[i].Interface.selected())
-            self.connect(self.LineUnActivates[i].widget, QtCore.SIGNAL("toggled(bool)"), self.LineUnActivates[i].Interface.select)
+            self.LineUnActivates[i].toggled.connect(self.LineUnActivates[i].Interface.selecte)
             self.LineMutes[i].widget.setChecked(self.LineMutes[i].Interface.selected())
-            self.connect(self.LineMutes[i].widget, QtCore.SIGNAL("toggled(bool)"), self.LineMutes[i].Interface.select)
+            self.LineMutes[i].widget.toggled.connect(self.LineMutes[i].Interface.select)
             self.LineGMutes[i].widget.setChecked(self.LineGMutes[i].Interface.selected())
-            self.connect(self.LineGMutes[i].widget, QtCore.SIGNAL("toggled(bool)"), self.LineGMutes[i].Interface.select)
+            self.LineMutes[i].widget.toggled.connect(self.LineGMutes[i].Interface.select)
             self.LineGDims[i].widget.setChecked(self.LineGDims[i].Interface.selected())
-            self.connect(self.LineGDims[i].widget, QtCore.SIGNAL("toggled(bool)"), self.LineGDims[i].Interface.select)
+            self.LineGDims[i].widget.toggled.connect(self.LineGDims[i].Interface.select)
             self.LineVolumes[i].widget.setValue(self.LineVolumes[i].Interface.getvalue())
-            self.connect(self.LineVolumes[i].widget, QtCore.SIGNAL("valueChanged(int)"), self.LineVolumes[i].Interface.setvalue)
+            self.LineVolumes[i].widget.valueChanged.connect(self.LineVolumes[i].Interface.setvalue)
  
         # HW switch controls the possibility of monitoring each output separatly 
         widget.HWSwitch.setChecked(self.HWselected())
-        self.connect(widget.HWSwitch, QtCore.SIGNAL("toggled(bool)"), self.HWToggle)
+        widget.HWSwitch.toggled.connect(self.HWToggle)
 
         # Line Out monitoring enabling depends on H/W switch
         self.LineOut = widget.LineOut

support/mixer-qt4/ffado/ffadowindow.py
@@ -28,7 +28,7 @@ from ffado.config import *
 
 import subprocess
 
-from PyQt4.QtCore import SIGNAL, SLOT, QObject, QTimer, Qt
+from PyQt4.QtCore import QObject, QTimer, Qt
 from PyQt4.QtGui import *
 
 from ffado.dbus_util import *
@@ -63,39 +63,39 @@ class FFADOWindow(QMainWindow):
         logging.getLogger('').addHandler(self.statuslogger)
 
         self.manager = PanelManager(self)
-        self.connect(self.manager, SIGNAL("connectionLost"), self.connectToDBUS)
+        self.manager.connectionLost.connect(self.connectToDBUS)
 
         filemenu = self.menuBar().addMenu("File")
         self.openaction = QAction("Open", self)
         self.openaction.setShortcut(self.tr("Ctrl+O"))
         self.openaction.setEnabled(False)
-        self.connect(self.openaction, SIGNAL("triggered()"), self.manager.readSettings)
+        self.openaction.triggered.connect(self.manager.readSettings)
         filemenu.addAction(self.openaction)
         self.saveaction = QAction("Save as...", self)
         self.saveaction.setShortcut(self.tr("Ctrl+S"))
         self.saveaction.setEnabled(False)
-        self.connect(self.saveaction, SIGNAL("triggered()"), self.manager.saveSettings)
+        self.saveaction.triggered.connect(self.manager.saveSettings)
         filemenu.addAction(self.saveaction)
         quitaction = QAction("Quit", self)
         quitaction.setShortcut(self.tr("Ctrl+q"))
-        self.connect(quitaction, SIGNAL("triggered()"), self, SLOT("close()"))
+        quitaction.triggered.connect(self.close)
         filemenu.addAction(quitaction)
 
         editmenu = self.menuBar().addMenu("Edit")
         self.updateaction = QAction("Update Mixer Panels", self)
         self.updateaction.setEnabled(False)
-        self.connect(self.updateaction, SIGNAL("triggered()"), self.manager.updatePanels)
+        self.updateaction.triggered.connect(self.manager.updatePanels)
         editmenu.addAction(self.updateaction)
         refreshaction = QAction("Refresh Current Panels", self)
-        self.connect(refreshaction, SIGNAL("triggered()"), self.manager.refreshPanels)
+        refreshaction.triggered.connect(self.manager.refreshPanels)
         editmenu.addAction(refreshaction)
 
         helpmenu = self.menuBar().addMenu( "Help" )
         aboutaction = QAction( "About FFADO", self )
-        self.connect( aboutaction, SIGNAL( "triggered()" ), self.aboutFFADO )
+        aboutaction.triggered.connect(self.aboutFFADO)
         helpmenu.addAction( aboutaction )
         aboutqtaction = QAction( "About Qt", self )
-        self.connect( aboutqtaction, SIGNAL( "triggered()" ), qApp, SLOT( "aboutQt()" ) )
+        aboutqtaction.triggered.connect(qApp.aboutQt)
         helpmenu.addAction( aboutqtaction )
 
         log.info( "Starting up" )
@@ -119,7 +119,7 @@ class FFADOWindow(QMainWindow):
             log.error("Could not communicate with the FFADO DBus service...")
             if not hasattr(self,"retry"):
                 self.retry = StartDialog(self)
-                self.connect(self.retry.button, SIGNAL("clicked()"), self.tryStartDBUSServer)
+                self.retry.button.clicked.connect(self.tryStartDBUSServer)
             if hasattr(self, "retry"):
                 self.manager.setParent(None)
                 self.setCentralWidget(self.retry)

support/mixer-qt4/ffado/logginghandler.py
@@ -21,20 +21,21 @@
 #
 
 from PyQt4.QtGui import QTextEdit, QAbstractSlider, QColor
-from PyQt4.QtCore import QObject, SIGNAL, SLOT
+from PyQt4.QtCore import QObject, pyqtSignal, QString
 
 import logging
 log = logging.getLogger('logginghandler')
 
 class QStatusLogger( QObject, logging.Handler ):
+    log = pyqtSignal(QString, int, name='log')
     def __init__( self, parent, statusbar, level=logging.NOTSET ):
         QObject.__init__( self, parent )
         logging.Handler.__init__( self, level )
         self.setFormatter( logging.Formatter( "%(name)s: %(message)s" ) )
-        self.connect( self, SIGNAL("log(QString,int)"), statusbar, SLOT("showMessage(QString,int)") )
+        self.log.connect(statusbar.showMessage)
 
     def emit( self, record ):
-        QObject.emit( self, SIGNAL("log(QString,int)"), "%s: %s" % (record.name, record.getMessage()), 5000 )
+        self.log.emit('%s: %s'.format(record.name, record.getMessage()), 5000)
 
 class QTextLogger( logging.Handler ):
     def __init__( self, parent, level=logging.NOTSET ):

support/mixer-qt4/ffado/mixer/audiofire.py
@@ -20,7 +20,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from PyQt4.QtCore import SIGNAL, SLOT, QObject, Qt, QTimer
+from PyQt4.QtCore import Qt, QTimer
 from PyQt4.QtGui import QWidget, QHBoxLayout, QVBoxLayout, \
                         QGroupBox, QTabWidget, QLabel, \
                         QPushButton, QToolButton, QSpacerItem, QSizePolicy
@@ -359,7 +359,7 @@ class AudioFire(QWidget):
             ctrl.setValue(vol)
 
             # connect the UI element
-            QObject.connect(ctrl,SIGNAL('valueChanged(int)'),self.updateMatrixVolume)
+            ctrl.valueChanged.connect(self.updateMatrixVolume)
 
         for ctrl, info in self.MatrixButtonControls.iteritems():
             state = self.hw.getMatrixMixerValue(self.MatrixButtonControls[ctrl][0],
@@ -373,7 +373,7 @@ class AudioFire(QWidget):
                 ctrl.setChecked(False)
 
             # connect the UI element
-            QObject.connect(ctrl,SIGNAL('clicked(bool)'),self.updateMatrixButton)
+            ctrl.clicked.connect(self.updateMatrixButton)
 
         for ctrl, info in self.MatrixRotaryControls.iteritems():
             vol = self.hw.getMatrixMixerValue(self.MatrixRotaryControls[ctrl][0],
@@ -384,7 +384,7 @@ class AudioFire(QWidget):
             ctrl.setValue(vol)
 
             # connect the UI element
-            QObject.connect(ctrl,SIGNAL('valueChanged(int)'),self.updateMatrixRotary)
+            ctrl.valueChanged.connect(self.updateMatrixRotary)
 
         for ctrl, info in self.VolumeControls.iteritems():
             vol = self.hw.getContignuous(self.VolumeControls[ctrl][0])
@@ -394,7 +394,7 @@ class AudioFire(QWidget):
             ctrl.setValue(vol)
 
             # connect the UI element
-            QObject.connect(ctrl,SIGNAL('valueChanged(int)'),self.updateVolume)
+            ctrl.valueChanged.connect(self.updateVolume)
 
         for ctrl, info in self.SelectorControls.iteritems():
             state = self.hw.getDiscrete(self.SelectorControls[ctrl][0])
@@ -405,11 +405,11 @@ class AudioFire(QWidget):
                 ctrl.setChecked(False)
 
             # connect the UI element
-            QObject.connect(ctrl,SIGNAL('clicked(bool)'),self.updateSelector)
+            ctrl.clicked.connect(self.updateSelector)
 
         for ctrl, info in self.TriggerControls.iteritems():
             # connect the UI element
-            QObject.connect(ctrl,SIGNAL('clicked()'),self.updateTrigger)
+            ctrl.clicked.connect(self.updateTrigger)
 
         for ctrl, info in self.SPDIFmodeControls.iteritems():
             state = self.hw.getDiscrete(self.SPDIFmodeControls[ctrl][0])
@@ -420,7 +420,7 @@ class AudioFire(QWidget):
                 ctrl.setChecked(False)
 
             # connect the UI element
-            QObject.connect(ctrl,SIGNAL('toggled(bool)'),self.updateSPDIFmodeControl)
+            ctrl.toggled.connect(self.updateSPDIFmodeControl)
 
         for ctrl, info in self.DigIfaceControls.iteritems():
             state = self.hw.getDiscrete(self.DigIfaceControls[ctrl][0])
@@ -428,17 +428,17 @@ class AudioFire(QWidget):
             if state > 0:
                 state -= 1
             ctrl.setCurrentIndex(state)
-            QObject.connect(ctrl, SIGNAL('activated(int)'), self.updateDigIfaceControl)
+            ctrl.activated.connect(self.updateDigIfaceControl)
 
         for ctrl, info in self.PlbkRouteControls.iteritems():
             sink = self.PlbkRouteControls[ctrl][1]
             src = self.hw.getDiscrete(self.PlbkRouteControls[ctrl][0], sink)
             ctrl.setCurrentIndex(src)
             self.setStreamLabel(src, sink)
-            QObject.connect(ctrl, SIGNAL('activated(int)'), self.updatePlbkRouteControl)
+            ctrl.activated.connect(self.updatePlbkRouteControl)
 
         self.update_timer = QTimer(self)
-        QObject.connect(self.update_timer, SIGNAL('timeout()'), self.polledUpdate)
+        self.update_timer.timeout.connect(self.polledUpdate)
         self.update_timer.start(1000)
         self.streaming_state = -1
 

support/mixer-qt4/ffado/mixer/bcoaudio5control.py
@@ -20,7 +20,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from PyQt4.QtCore import SIGNAL, SLOT, QObject
 from PyQt4.QtGui import QWidget
 from ffado.config import *
 import logging

support/mixer-qt4/ffado/mixer/dummy.py
@@ -20,7 +20,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from PyQt4.QtCore import SIGNAL, SLOT, QObject
 from PyQt4.QtGui import QWidget
 from ffado.config import *
 

support/mixer-qt4/ffado/mixer/edirolfa101control.py
@@ -20,7 +20,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from PyQt4.QtCore import SIGNAL, SLOT, QObject
 from PyQt4.QtGui import QWidget
 from ffado.config import *
 

support/mixer-qt4/ffado/mixer/edirolfa66control.py
@@ -20,7 +20,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from PyQt4.QtCore import SIGNAL, SLOT, QObject
 from PyQt4.QtGui import QWidget
 from ffado.config import *
 

commit 0005e86f0ee1182dfc384f7081d310ef233961fb

saffire_dice: fix a typo in the Saffire Dice mixer python code introduced in r2653 which caused the global mute buttons for the line outs in the monitoring tab not to work.  Thanks to Nils Philippsen for noticing this, tracking down the cause and providing the patch to fix it.
git-svn-id: http://subversion.ffado.org/ffado/trunk/libffado@2676 2be59082-3212-0410-8809-b0798e1608f0

support/mixer-qt4/ffado/mixer/saffire_dice.py
@@ -315,7 +315,7 @@ class Saffire_Dice(Generic_Dice_EAP):
             self.LineMutes[i].widget.setChecked(self.LineMutes[i].Interface.selected())
             self.LineMutes[i].widget.toggled.connect(self.LineMutes[i].Interface.select)
             self.LineGMutes[i].widget.setChecked(self.LineGMutes[i].Interface.selected())
-            self.LineMutes[i].widget.toggled.connect(self.LineGMutes[i].Interface.select)
+            self.LineGMutes[i].widget.toggled.connect(self.LineGMutes[i].Interface.select)
             self.LineGDims[i].widget.setChecked(self.LineGDims[i].Interface.selected())
             self.LineGDims[i].widget.toggled.connect(self.LineGDims[i].Interface.select)
             self.LineVolumes[i].widget.setValue(self.LineVolumes[i].Interface.getvalue())