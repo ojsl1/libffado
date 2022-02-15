3ccd6ff8df9cfb2e2cc1b23fc249e79524a8be2e
jwoithe <jwoithe@2be59082-3212-0410-8809-b0798e1608f0>
2022-02-15 10:05:14 +0000

    ControlInterface: int typecasts for getDiscrete() and getMatrixMixerValue().
    Add explicit typecasts to int to work around type-related changes seemingly
    introduced in python 3.10.  These changes are closely related to those in
    r2817.  Thanks to Nils Philippsen for drawing our attention to these methods
    and their need for the typecast.
    
    
    git-svn-id: http://subversion.ffado.org/ffado/trunk/libffado@2820 2be59082-3212-0410-8809-b0798e1608f0

--- support/mixer-qt4/ffado/dbus_util.py
+++ support/mixer-qt4/ffado/dbus_util.py
@@ -100,9 +100,9 @@ class ControlInterface:
             dev = self.bus.get_object(self.servername, path)
             dev_cont = dbus.Interface(dev, dbus_interface='org.ffado.Control.Element.Discrete')
             if idx == None:
-                return dev_cont.getValue()
+                return int(dev_cont.getValue())
             else:
-                return dev_cont.getValueIdx(idx)
+                return int(dev_cont.getValueIdx(idx))
         except:
             log.error("Failed to get Discrete %s on server %s" % (path, self.servername))
             return 0
@@ -152,7 +152,7 @@ class ControlInterface:
         try:
             dev = self.bus.get_object(self.servername, path)
             dev_cont = dbus.Interface(dev, dbus_interface='org.ffado.Control.Element.MatrixMixer')
-            return dev_cont.getValue(row, col)
+            return int(dev_cont.getValue(row, col))
         except:
             log.error("Failed to get MatrixMixer %s on server %s" % (path, self.servername))
             return 0
